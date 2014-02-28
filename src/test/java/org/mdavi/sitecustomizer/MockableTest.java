086315cfa01ff190eba9d2eaac4a69db264820d0
Marco Davi <mardavi84@gmail.com>
2014-02-28 11:31:25 +0100

    Fixed gradle path, solved JUnitRuleMockery issue

--- src/test/java/org/mdavi/sitecustomizer/MockableTest.java
+++ src/test/java/org/mdavi/sitecustomizer/MockableTest.java
@@ -1,15 +1,9 @@
 package org.mdavi.sitecustomizer;
 
 import org.jmock.integration.junit4.JUnitRuleMockery;
-import org.junit.After;
 import org.junit.Rule;
 
 public abstract class MockableTest extends FakeCobrandTest
 {
   @Rule public JUnitRuleMockery context = new JUnitRuleMockery();
-  
-  @After
-  public void teardown() {
-    context.assertIsSatisfied();
-  }
 }
